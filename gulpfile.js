063412c1a390fd56866e4491276952f27d9f75aa
Jamie Lee <jamie.lee@waterstones.com>
2017-11-20 20:22:53 +0000

    add gulp-util, and other gulpfile updates

--- gulpfile.js
+++ gulpfile.js
@@ -2,22 +2,25 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
+var gutil = require('gulp-util');
 
 gulp.task('styles', function() {
     gulp.src('scss/**/*.scss')
         .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(concat('styles.min.css'))
         .pipe(gulp.dest('.'))
 });
 
 gulp.task('scripts', function() {
   return gulp.src(['js/*.js'])
+    .pipe(uglify().on('error', gutil.log))
     .pipe(concat('scripts.min.js'))
-    .pipe(uglify())
     .pipe(gulp.dest('.'));
 });
 
-//Watch task
-gulp.task('default',function() {
+gulp.task('watch',function() {
     gulp.watch('scss/**/*.scss',['styles']);
     gulp.watch('js/**/*.js',['scripts']);
 });
+
+gulp.task('default', ['watch', 'scripts', 'styles']);
